c3c82833b675581cac61785d25ae4c4a2fe2abc7
mustafaakurt <mustafaakurt@yandex.com>
2024-05-17 19:49:21 +0300

    Initials added

---
+++ 01-basic-programming-concepts/HelloWorld/src/Initials.java
@@ -0,0 +1,12 @@
+public class Initials {
+    public static void main(String[] args) {
+        System.out.println("**\t\t\t\t**\t**\t\t\t\t**\n");
+        System.out.println("**\t**\t\t**\t**\t**\t\t\t**\n");
+        System.out.println("**\t\t**\t\t**\t**\t\t**\n");
+        System.out.println("**\t\t\t\t**\t**\t**\n");
+        System.out.println("**\t\t\t\t**\t**\t**\n");
+        System.out.println("**\t\t\t\t**\t**\t\t**\n");
+        System.out.println("**\t\t\t\t**\t**\t\t\t**\n");
+        System.out.println("**\t\t\t\t**\t**\t\t\t\t**\n");
+    }
+}
