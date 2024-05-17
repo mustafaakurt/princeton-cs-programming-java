990bf91f5a69dd5fc7982b0ec11700b4ba3374dc
mustafaakurt <mustafaakurt@yandex.com>
2024-05-17 19:41:09 +0300

    HelloWorldMultilingual added

---
+++ 01-basic-programming-concepts/HelloWorld/src/HelloWorldMultilingual.java
@@ -0,0 +1,7 @@
+public class HelloWorldMultilingual {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Dünya");
+        System.out.println("Hello World");
+        System.out.println("\uD83D\uDC4B\uD83C\uDF0E");
+    }
+}
